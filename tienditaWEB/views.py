e1b1859a2a903b7425d9a8c5488b27ce9b760205
Emanuel Real <malayadelossiglos7@gmail.com>
2023-10-16 23:04:27 -0600

    modulo 3

--- tienditaWEB/views.py
+++ tienditaWEB/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
+from .carrito import Cart
 
 # Create your views here.
 """VISTAS PARA EL CATALOGO DEL PRODUCTO """
@@ -58,3 +59,47 @@ def productoDetalle(request,producto_id):
     }
 
     return render(request,'producto.html',context)
+
+
+"""VISTAS PARA EL CARRITO DE COMPRAS"""
+
+
+
+def carrito(request):
+    return render(request,'carrito.html')
+
+
+def agregarCarrito(request,producto_id):
+    
+    if request.method == 'POST':
+          cantidad = int(request.POST['cantidad'])
+
+    else:
+        cantidad = 1
+        
+
+    objProducto = Producto.objects.get(pk=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.add(objProducto,cantidad)
+    #print(request.session.get("cart"))
+
+    if request.method == 'GET':
+        return redirect('/')
+
+    return render(request,'carrito.html')
+
+
+def eliminarProductoCarrito(request,producto_id):
+    objProducto = Producto.objects.get(pk=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.delete(objProducto)
+
+    return render(request,'carrito.html')
+
+def limpiarCarrito(request):
+    carritoProducto = Cart(request)
+    carritoProducto.clear()
+
+    return render(request,'carrito.html')
+
+
